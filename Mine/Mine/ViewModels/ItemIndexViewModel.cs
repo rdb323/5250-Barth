31a31df6978f0d5b476a92046754cec7acbc5fa2
rdb323 <barthr@seattleu.edu>
2021-01-28 18:43:54 -0800

    Added Toolbar to Update Page and event method, in ItemReadPage.xaml, ItemReadPage.xaml.cs

--- Mine/Mine/ViewModels/ItemIndexViewModel.cs
+++ Mine/Mine/ViewModels/ItemIndexViewModel.cs
@@ -33,6 +33,12 @@ namespace Mine.ViewModels
                 var data = item as ItemModel;
                 await DeleteAsync(data);
             });
+
+            MessagingCenter.Subscribe<ItemUpdatePage, ItemModel>(this, "UpdateItem", async (obj, item) =>
+            {
+                var data = item as ItemModel;
+                await UpdateAsync(data);
+            });
         }
 
         async Task ExecuteLoadItemsCommand()
@@ -78,7 +84,7 @@ namespace Mine.ViewModels
         /// </summary>
         /// <param name="data">The Record to Delete</param>
         /// <returns>True if deleted</returns>
-        async Task<bool> DeleteAsync(ItemModel data)
+        public async Task<bool> DeleteAsync(ItemModel data)
         {
             //Check if the record exists, if not returns false
             var record = await ReadAsync(data.Id);
@@ -92,6 +98,29 @@ namespace Mine.ViewModels
 
             //Call to remove it from the Data Store
             var result = await DataStore.DeleteAsync(data.Id);
+
+            return result;
+        }
+
+        /// <summary>
+        /// Update the record from the system
+        /// </summary>
+        /// <param name="data">The Record to Update</param>
+        /// <returns>True if Updated</returns>
+        public async Task<bool> UpdateAsync(ItemModel data)
+        {
+            //Check if the record exists, if not returns false
+            var record = await ReadAsync(data.Id);
+            if (record == null)
+            {
+                return false;
+            }
+
+            //Update from the local data set cache
+            var result = await DataStore.UpdateAsync(data);
+
+            var canExecute = LoadItemsCommand.CanExecute(null);
+            LoadItemsCommand.Execute(null);
 
             return result;
         }
