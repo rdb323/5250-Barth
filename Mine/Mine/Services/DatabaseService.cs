46901f4ee1c1f97becb8fd50c7e14530117d1ea5
rdb323 <barthr@seattleu.edu>
2021-02-03 19:24:27 -0800

    Added Delete to DatabaseService.cs

--- Mine/Mine/Services/DatabaseService.cs
+++ Mine/Mine/Services/DatabaseService.cs
@@ -75,9 +75,26 @@ namespace Mine.Services
             return true;
         }
 
-        public Task<bool> DeleteAsync(string id)
+        /// <summary>
+        /// Deletes the given id if viable
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async Task<bool> DeleteAsync(string id)
         {
-            throw new NotImplementedException();
+            var data = await ReadAsync(id);
+            if(data == null)
+            {
+                return false;
+            }
+
+            var result = await Database.DeleteAsync(data);
+            if(result == 0)
+            {
+                return false;
+            }
+
+            return false;
         }
 
         /// <summary>
