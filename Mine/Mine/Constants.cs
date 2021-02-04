04a6c5734cb7f680499953b60e0fff2b37eb891e
rdb323 <barthr@seattleu.edu>
2021-02-03 16:25:42 -0800

    Added Flags to open, create, and thread database use to Constants.cs

--- Mine/Mine/Constants.cs
+++ Mine/Mine/Constants.cs
@@ -7,5 +7,16 @@ namespace Mine
     class Constants
     {
         public const string DatabaseFilename = "TodoSQLite.db3";
+
+        public const SQLite.SQLiteOpenFlags Flags =
+            //open the database in read/write mode
+            SQLite.SQLiteOpenFlags.ReadWrite |
+            //create the database if it doesn't exist
+            SQLite.SQLiteOpenFlags.Create |
+            //enable multi-threaded database access
+            SQLite.SQLiteOpenFlags.SharedCache;
+
+
+
     }
 }
