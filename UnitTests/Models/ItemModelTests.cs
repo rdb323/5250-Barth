b9b34751116a9bfeeb328521e31c0d466831917b
rdb323 <barthr@seattleu.edu>
2021-02-10 16:10:41 -0800

    Added ItemModelTests.cs to the project

---
+++ UnitTests/Models/ItemModelTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace UnitTests.Models
+{
+    [TestFixture]
+    class ItemModelTests
+    {
+        [Test]
+        public void ItemModel_Constructor_Valid_()
+        {
+            //Arrange
+
+            //Act
+
+            //Reset
+
+            //Assert
+            Assert.Pass();
+        }
+    }
+}
