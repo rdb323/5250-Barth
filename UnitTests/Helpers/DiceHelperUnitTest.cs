d7b599ff09eb48483fb2dd3a1e14c274087d7b5e
rdb323 <barthr@seattleu.edu>
2021-02-10 17:08:24 -0800

    Added RollDice_Valid_Roll_1_Dice_10_Fixed_5_Should_Return_5 to DiceHelperUnitTest.cs

--- UnitTests/Helpers/DiceHelperUnitTest.cs
+++ UnitTests/Helpers/DiceHelperUnitTest.cs
@@ -38,5 +38,23 @@ namespace UnitTests.Helpers
             Assert.AreEqual(true, result >= 1);
             Assert.AreEqual(true, result <= 6);
         }
+
+        [Test]
+        public void RollDice_Valid_Roll_1_Dice_10_Fixed_5_Should_Return_5()
+        {
+            //Arrange
+            DiceHelper.ForceRollsToNotRandom = true;
+            DiceHelper.ForcedRandomValue = 5;
+
+
+            //Act
+            var result = DiceHelper.RollDice(1, 10);
+
+            //Reset
+            DiceHelper.ForceRollsToNotRandom = false;
+
+            //Assert
+            Assert.AreEqual(5, result);
+        }
     }
 }
